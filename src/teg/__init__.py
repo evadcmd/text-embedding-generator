b7cc697436a778059b69034dc0d4809b05040ff1
minta.chiu <minta.chu@labbase.co.jp>
2024-08-24 14:34:45 +0900

    add log settings

--- src/teg/__init__.py
+++ src/teg/__init__.py
@@ -1,4 +1,10 @@
 import torch
+import logging.config
+import concurrent_log_handler  # pyright: ignore #noqa F401
 
+logging.config.fileConfig("./log.conf", disable_existing_loggers=False)
+
+
+logger = logging.getLogger("root")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
+logger.info(f"Using device: {device}")
