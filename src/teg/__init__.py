afbf35e143bb860b98c6c5affbf19c91451f2ca9
minta.chiu <minta.chu@labbase.co.jp>
2024-08-28 15:58:17 +0900

    add mps device settings

--- src/teg/__init__.py
+++ src/teg/__init__.py
@@ -7,5 +7,11 @@ logging.config.fileConfig("./log.conf", disable_existing_loggers=False)
 
 
 logger = logging.getLogger("root")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device(
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
 logger.info(f"Using device: {device}")
